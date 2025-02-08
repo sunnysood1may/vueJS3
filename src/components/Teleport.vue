e4117226881362fdc2e1c5224ec83edb8e096660
sunny sood <sunny1may@gmail.com>
2025-02-08 09:41:59 +0530

    Teleport footer

---
+++ src/components/Teleport.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>TelePort component</h2>  
+    <teleport to="#footer">
+        <FooTer />
+    </teleport>
+</template>
+
+<script>
+import FooTer from './Footer.vue';
+export default {
+    name: 'TelePort',
+    components: {
+        FooTer
+    }
+}
+</script>
+<style scoped>
+h2 {
+    color: orange;
+}
+</style>
+
